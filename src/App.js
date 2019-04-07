15a4d3c548450d10b04e00af055ee12f40a5da11
marasva <mariesva@stud.ntnu.no>
2019-04-07 14:56:09 -0400

    Test Heroku and Github connection

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Testing things works, now I have editted <code>src/App.js</code> and saved.
           </p>
           <a
             className="App-link"
