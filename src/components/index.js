ccbec0f03d58774fd967d5b261c8d07b6d970ec0
Torsten Moss <tbm94@live.no>
2019-06-29 17:01:22 -0400

    Other pointless commit to test github hooks

--- src/components/index.js
+++ src/components/index.js
@@ -25,7 +25,7 @@ class Index extends Component {
               <img src={logo2} className="App-logo" alt="logo" />
             </div>
             <div className="title">
-              Welcome! Please choose from the options below
+              Please choose from the options below
             </div>
             <Router>
               <div className="buttons">
