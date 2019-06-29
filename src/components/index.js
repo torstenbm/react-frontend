eed395be5d4cfb4880a06e6112343d64fe4b09e9
Torsten Moss <tbm94@live.no>
2019-06-29 16:53:52 -0400

    Stop being tryhard

--- src/components/index.js
+++ src/components/index.js
@@ -29,8 +29,8 @@ class Index extends Component {
             </div>
             <Router>
               <div className="buttons">
-                <a className="button bias-button" href="/biases/">Analyze wikipedia!</a>
-                <a className="button cg-button" href="/content-gaps/">Try the SPARQL explorer!</a>
+                <a className="button bias-button" href="/biases/">Analyze wikipedia</a>
+                <a className="button cg-button" href="/content-gaps/">Try the SPARQL explorer</a>
               </div>
             </Router>
             <button className="logout button" onClick={this.logout}>Log out</button>
